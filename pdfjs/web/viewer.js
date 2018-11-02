4060e51921032924c677ef70d9179ee9089bec4a
Georg Schelkshorn <georg.schelkshorn@touchlay.com>
2018-11-02 18:47:54 +0100

    Change DEFAULT_SCALE_VALUE

--- pdfjs/web/viewer.js
+++ pdfjs/web/viewer.js
@@ -1471,7 +1471,7 @@ var pdfjsWebLibs;
       factory(root.pdfjsWebUIUtils = {}, root.pdfjsWebPDFJS);
     }(this, function (exports, pdfjsLib) {
       var CSS_UNITS = 96.0 / 72.0;
-      var DEFAULT_SCALE_VALUE = 'auto';
+      var DEFAULT_SCALE_VALUE = 'page-fit';
       var DEFAULT_SCALE = 1.0;
       var UNKNOWN_SCALE = 0;
       var MAX_AUTO_SCALE = 1.25;
